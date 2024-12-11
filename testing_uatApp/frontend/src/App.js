8d49da02bed9d05a82d89510e5d42e92a3fcc37c
fouad mahmoud <fouad_31057119@f-eng.tanta.edu.eg>
2024-12-11 18:47:12 +0200

    Update files

---
+++ testing_uatApp/frontend/src/App.js
@@ -0,0 +1,37 @@
+// App.js
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+        </nav>
+        
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+
+        <footer>
+          &copy; 2024 testing_uat App. All rights reserved
+        </footer>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
